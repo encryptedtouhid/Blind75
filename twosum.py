966492aebbee43213383e18dc774bba35205b6e1
Khaled <me@tuhidulhossain.com>
2024-06-25 22:31:12 +0800

    #1  solved with python

--- twosum.py
+++ twosum.py
@@ -1,2 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+from typing import List
+
+def main():
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = twoSum(nums, target)
+    print(result)
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    
+    totalItem = len(nums)
+    for num in range(totalItem):
+         for rnum in range(num+1, totalItem):
+                if(nums[num] + nums[rnum] == target):
+                    return [num, rnum]
+    
+if __name__ == "__main__":
+    main()
